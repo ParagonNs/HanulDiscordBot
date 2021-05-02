de3cdb491061ac55f594001d511ec3096ddac216
ParagonNs <83499084+ParagonNs@users.noreply.github.com>
2021-05-02 20:48:57 +0900

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,11 +17,10 @@ async def hello(ctx):
 
 @bot.command()
 async def 주사위(ctx):
-    await ctx.send("hello")
     a = random.randrange(1,7)
     embed = discord.Embed(title = "주사위 굴리기 결과", description = None, color = 0xFAFA00)
-    embed.add_field(name = ctx.author.name+"의 숫자", value = ":game_die: ", inline = True)
-    embed.set_footer(text="주사위 결과: " + str(a))
+    embed.add_field(ctx.author.name+"의 숫자", ":game_die: " + str(a), inline = True)
+    embed.set_footer("주사위 결과: " + str(a))
     await ctx.send(embed=embed)
 
 @bot.event

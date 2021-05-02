80cbbd3481d1ffb1f1fcf2c1a7c7fece60332ba3
ParagonNs <83499084+ParagonNs@users.noreply.github.com>
2021-05-02 16:38:52 +0900

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,22 @@
 # -*_ coding: utf-8 -*-
 
-import discord
+import asyncio, discord
 from discord.ext import commands
+
+bot = commands.Bot(command_prefix="&")
+
+@bot.event
+async def on_ready():
+	print("We have loggedd in as {0.user}".format(bot))
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("hello")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+    	await ctx.send("Command not found")
+        
+
+bot.run('token')

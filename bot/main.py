739b5e26ec054ec2aaaaeedf92445a5df51fe0e5
ParagonNs <83499084+ParagonNs@users.noreply.github.com>
2021-05-02 16:42:49 +0900

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,8 @@
 # -*_ coding: utf-8 -*-
+#main
 
 import asyncio, discord
+from simpledice import *
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="&")
@@ -16,7 +18,6 @@ async def hello(ctx):
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-    	await ctx.send("Command not found")
-        
+    	await ctx.send("없거나 잘못된 명령어입니다.")
 
 bot.run('token')

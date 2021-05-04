285b3d0e3efc0a64bbb931639891e2f3a39bc01b
ParagonNs <83499084+ParagonNs@users.noreply.github.com>
2021-05-04 20:56:36 +0900

    Update user.py

--- bot/user.py
+++ bot/user.py
@@ -1,1 +1,23 @@
+#user.py
 
+from openpyxl import load_workbook, Workbook
+
+#Data list
+u_id = 1 #유저의 id
+u_name = 2 #유저의 이름
+u_credit = 3 #유저의 크레딧
+u_level = 4 #유저의 레벨
+#Default Data
+default_credit = 100
+default_level = 1
+
+wb = load_workbook("userDB.xlsx")
+ws = wb.active
+
+def signup(_name, _id):
+    ws.cell(row=2, column=u_id, value=_name)
+    ws.cell(row=2, column=u_name, value =_id)
+    ws.cell(row=2, column=u_credit, value = default_credit)
+    ws.cell(row=2, column=u_level, value = defailt_level)
+
+    wb.save("userDB.xlsx")

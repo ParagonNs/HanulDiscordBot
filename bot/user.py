d09d94e30215ffc6069961476a7b3fb0f5b874de
ParagonNs <83499084+ParagonNs@users.noreply.github.com>
2021-05-05 10:38:22 +0900

    Update user.py

--- bot/user.py
+++ bot/user.py
@@ -6,11 +6,13 @@ from openpyxl import load_workbook, Workbook
 u_id = 1 #유저의 id
 u_name = 2 #유저의 이름
 u_credit = 3 #유저의 크레딧
-u_level = 4 #유저의 레벨
+u_level = 4 #유저의 레벨( 메인 )
 u_aresource = 5#유저가 가진 자원 A
 u_bresource = 6#유저가 가진 자원 B
 u_cresource = 7#유저가 가진 자원 C
 u_dresource = 8#유저가 가진 자원 D
+u_levelmining = 9 #유저의 채굴 레벨
+u_miningcountupgrade = 10 #채굴 횟수 업그레이드
 #Default Data
 default_credit = 100
 default_level = 1
